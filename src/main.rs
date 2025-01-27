4f13d2c24a9dbc192079dba7ecc8ce7191537bb6
Taine Reader <taine.reader@gmail.com>
2025-01-27 20:59:50 +1300

    updates before fedora nuke

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use http::{Request, Response, StatusCode};
+use octocrab::*;
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = "InvalidSE";
+    let user2 = "Hoverth";
+    let mut connections: Vec<Vec<String>> = Vec::new();
+    
+    loop {
+        
+    }
+}
+
+fn get_following(user: &str) {
+    
+}
+
+fn get_followers(user: &str) {
+
+}
+
+fn get_repo_collaborators() {
+    // Not implemented
+}
+
+fn get_forks() {
+    // Not implemented
 }
